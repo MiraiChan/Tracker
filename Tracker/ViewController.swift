8e96189cbf17ed95d8a6fb26a46d386e421a1f44
Almira Khafizova <mimirka80@gmail.com>
2023-10-26 22:27:24 +0200

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Almira Khafizova on 26.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
