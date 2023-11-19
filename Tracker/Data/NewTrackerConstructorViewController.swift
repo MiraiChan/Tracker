07c75ac3f31f940dced250c920f90649b2486d8b
Almira Khafizova <mimirka80@gmail.com>
2023-11-19 20:38:35 +0100

    Deleted unnecessary files.

--- Tracker/Data/NewTrackerConstructorViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NewTrackerConstructorViewController.swift
-//  Tracker
-//
-//  Created by Almira Khafizova on 08.11.23.
-//
-
-import Foundation
--- Tracker/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  Tracker
-//
-//  Created by Almira Khafizova on 08.11.23.
-//
-
-import Foundation
