e10c1aea45daacfc35470d8cdfcc4c0bff7f25e7
Almira Khafizova <mimirka80@gmail.com>
2023-11-24 00:10:14 +0100

    NSPersistentContainer was initialized.

--- Tracker/Application/AppDelegate.swift
+++ Tracker/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -29,5 +30,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TrackersEntity")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                assert(false, "Unresolved error \(error)")
+            } else {
+                assert(true, "DB url - \(storeDescription.url!.absoluteString)")
+            }
+        })
+        return container
+    }()
+    
+    lazy var context: NSManagedObjectContext = {
+        return persistentContainer.viewContext
+    }()
 }
 
