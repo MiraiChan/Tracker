fa858f4d0d2ed285ad963b118b597c1df0347149
Almira Khafizova <mimirka80@gmail.com>
2023-11-26 00:45:39 +0100

    Protocols, according constants (to get data changes from Core Data) and class init-s were added to TrackerStore and TrackerRecordStore.

--- Tracker/CoreData/TrackerRecordStore.swift
+++ Tracker/CoreData/TrackerRecordStore.swift
@@ -7,3 +7,21 @@
 
 import UIKit
 import CoreData
+
+protocol TrackerRecordStoreDelegate: AnyObject {
+    func storeRecord()
+}
+
+final class TrackerRecordStore: NSObject {
+    private var context: NSManagedObjectContext
+    private var fetchedResultsController: NSFetchedResultsController<TrackerRecordCoreData>!
+    
+    weak var delegate: TrackerRecordStoreDelegate?
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+        super.init()
+    }
+}
+
+
--- Tracker/CoreData/TrackerStore.swift
+++ Tracker/CoreData/TrackerStore.swift
@@ -7,3 +7,19 @@
 
 import UIKit
 import CoreData
+
+protocol TrackerStoreDelegate: AnyObject {
+    func store() -> Void
+}
+
+final class TrackerStore: NSObject {
+    private var context: NSManagedObjectContext
+    private var fetchedResultsController: NSFetchedResultsController<TrackerCoreData>!
+    
+    weak var delegate: TrackerStoreDelegate?
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+        super.init()
+    }
+}
