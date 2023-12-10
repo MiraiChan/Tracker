635ea43ae631f60620c3cb2794f55e13b9c24b0b
Almira Khafizova <mimirka80@gmail.com>
2023-12-10 12:37:22 +0100

    fatalError replaced with assertionFailure, 'final' added to class PageViewController.

--- Tracker/Application/AppDelegate.swift
+++ Tracker/Application/AppDelegate.swift
@@ -10,6 +10,7 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    static let shared = AppDelegate()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
@@ -24,20 +25,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
     
-    lazy var persistentContainer: NSPersistentContainer = {
+    private lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Tracker")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                assert(false, "Unresolved error \(error)")
-            } else {
-                assert(true, "DB url - \(storeDescription.url!.absoluteString)")
+                assertionFailure("Failed to load DB!")
             }
         })
         return container
     }()
     
-    lazy var context: NSManagedObjectContext = {
+    var context: NSManagedObjectContext {
         return persistentContainer.viewContext
-    }()
+    }
+    
+    private override init() {}
 }
 
--- Tracker/Onboarding/PageViewController.swift
+++ Tracker/Onboarding/PageViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class PageViewController: UIViewController {
+final class PageViewController: UIViewController {
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-
+    
     private let label: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -24,28 +24,30 @@ class PageViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     init(imageName: String, labelText: String) {
         super.init(nibName: nil, bundle: nil)
         imageView.image = UIImage(named: imageName)
         label.text = labelText
     }
-
+    
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        assertionFailure("init(coder:) has not been implemented")
+        return nil
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(imageView)
         view.addSubview(label)
-
+        
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: view.topAnchor),
             imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-
+            
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.topAnchor.constraint(equalTo: view.topAnchor, constant: 452),
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
